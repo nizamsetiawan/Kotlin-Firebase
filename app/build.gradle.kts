56d985a846324dbe74352d913b2bc2701f96458e
M. Nizam Setiawan <nizamsetiawan15@gmail.com>
2024-06-30 02:25:11 +0700

    feat : add cloud messaging by firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
 
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
